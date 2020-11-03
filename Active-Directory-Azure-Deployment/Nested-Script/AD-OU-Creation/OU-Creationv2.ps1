047f93a393413f3f504acf535db3d62c401d38c5
Aldebarancloud <45065026+Aldebarancloud@users.noreply.github.com>
2020-11-03 15:00:55 +0100

    Update OU-Creationv2.ps1

--- Active-Directory-Azure-Deployment/Nested-Script/AD-OU-Creation/OU-Creationv2.ps1
+++ Active-Directory-Azure-Deployment/Nested-Script/AD-OU-Creation/OU-Creationv2.ps1
@@ -1,6 +1,6 @@
 param( 
-[parameter(Mandatory=$true)][string]$ouUsers,
-[parameter(Mandatory=$true)][string]$ouHosts
+[variables(Mandatory=$true)][string]$ouUsers,
+[variables(Mandatory=$true)][string]$ouHosts
 )
 
 New-ADOrganizationalUnit -Name $ouUsers
